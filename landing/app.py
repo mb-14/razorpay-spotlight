0235d5137c77f768f7cbf45d4a635764fdb413d1
Tasdik Rahman <git@tasdikrahman.me>
2020-12-11 19:31:53 +0530

    chore: landing: initial static page

--- landing/app.py
+++ landing/app.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def landing():
-    return "foo"
+    return render_template('index.html', title='Home')
