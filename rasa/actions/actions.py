c32a7eb1be83542bf21f50a40b0f90784d54cf7d
Miroojin Bakshi <mbakshi1094@gmail.com>
2020-12-12 16:04:58 +0530

    Checkpoint

--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -4,24 +4,50 @@
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
 
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from typing import Dict, Text, Any, List, Union, Optional
+from influxdb import InfluxDBClient
 
-# This is a simple example for a custom action which utters "Hello World!"
+client = InfluxDBClient('localhost', 8086, '', '', 'rzpftx')
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+measurements = {
+       "payments": "payment_authorized",
+       "failed_payments": "payment_failed"
+       }
+
+class ActionListMetrics(Action):
+   def name(self) -> Text:
+      return "action_list_metrics"
+
+   def run(self,
+           dispatcher: CollectingDispatcher,
+           tracker: Tracker,
+           domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+      result = ["payment_authorized (count/volume)", "payment_failure (count/volume)", "success_rate"]
+    
+      dispatcher.utter_message("The following metrics are available to query from: {}".format(", ".join(result)))
+      return []
+
+
+class ActionListDimensions(Action):
+
+   def name(self) -> Text:
+      return "action_list_dimensions"
+
+   def run(self,
+           dispatcher: CollectingDispatcher,
+           tracker: Tracker,
+           domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+      measurement = next(tracker.get_latest_entity_values("measurement"), None)
+      if measurement not in ["payments", "failed_payments"]:
+        dispatcher.utter_message("Could not recognize metric: {}".format(measurement))
+        return []
+
+      result = client.query("SHOW TAG KEYS FROM {}".format(measurements[measurement]))
+      points = list(result.get_points())
+      tags = map(lambda x: x["tagKey"], points)
+      dispatcher.utter_message("The following dimensions are available for {}: {}".format(measurement, ", ".join(tags)))
+      return []
