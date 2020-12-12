6347e6bf55a3db22045f218ca3470a186f215fac
Tasdik Rahman <git@tasdikrahman.me>
2020-12-12 17:10:43 +0530

    chore: fix typo while setting the error_step in generatePayloadJson method

--- webhook/mock-data-generator/generator.go
+++ webhook/mock-data-generator/generator.go
@@ -77,8 +77,8 @@ func generatePayloadJson(createdAt time.Time) json.Json {
 		template.Set(field.Path, []byte(fmt.Sprintf(`%d`, value)))
 	}
 	for _, error_field := range config.ErrorFields {
-		value := field.Values[rand.Intn(len(field.Values))]
-		template.Set(field.Path, str(value))
+		value := error_field.Values[rand.Intn(len(error_field.Values))]
+		template.Set(error_field.Path, str(value))
 	}
 
 	template.Set("payload.payment.entity.created_at", []byte(fmt.Sprintf(`%d`, createdAt.Unix())))
